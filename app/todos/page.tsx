e6032cb714a8ca47c42e6b2fc8d44099c9084435
massu-159 <magjtwjp@gmail.com>
2023-11-03 20:13:46 +0900

    [update] server action / delete

--- app/todos/page.tsx
+++ app/todos/page.tsx
@@ -1,3 +1,5 @@
+import DeleteButton from '@/components/delete-button';
+import { addTodo } from '@/lib/actions';
 import prisma from '@/lib/prisma';
 
 const Page = async () => {
@@ -8,9 +10,22 @@ const Page = async () => {
       <h1 className="text-xl font-bold">Todo一覧</h1>
       <ul className="mt-8">
         {todos.map((todo) => (
-          <li key={todo.id}>{todo.name}</li>
+          <li key={todo.id}>
+            <span>{todo.name}</span>
+            <DeleteButton id={todo.id} ></DeleteButton>
+          </li>
         ))}
       </ul>
+      <form className="flex items-center mt-4" action={addTodo}>
+        <label htmlFor="name">Name:</label>
+        <input type="text" name="name" className="border mx-2 p-1" />
+        <button
+          type="submit"
+          className="bg-blue-600 px-2 py-1 rounded-lg text-sm text-white"
+        >
+          Add Todo
+        </button>
+      </form>
     </div>
   );
 };
